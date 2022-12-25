19868204bee42706346cac641cd14c6cd69beaf5
Swarup Yeole <76248886+swarupyeole11@users.noreply.github.com>
2022-12-25 14:58:15 +0000

    Created the Block Class

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 def genHash():
-  
+
 
 
 class Block:
-
+   def __init__(self,data,hash,prevHash):
+     self.data=data;
+     self.hash=hash;
+     self.prevHash=prevHash;
 
 
 
 class Blockchain:
-
+   def __init__()
   
