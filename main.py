6a632a71385f77a1e16d8aaed5acd3089285e7d6
Swarup Yeole <76248886+swarupyeole11@users.noreply.github.com>
2022-12-26 02:51:40 +0000

    Made the basic structure of blockchain

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-def genHash():
+import hashlib
+def genHash(data):
+   result = hashLib.sha256(data.encode())
+   return result.hexdigest()
 
 
-
-class Block:
+class Block: #The fields of each new block being created 
    def __init__(self,data,hash,prevHash):
      self.data=data;
      self.hash=hash;
@@ -11,5 +13,15 @@ class Block:
 
 
 class Blockchain:
-   def __init__()
-  
+   def __init__(self):
+    hashLast = genHash('gen_last')
+    hashStart = genHash('gen_hash')
+
+    genesis=Block('Intial_Block',hashStart,hashLast)
+    self.chain = [genesis]  #defined a list in which the blocks will get stored 
+
+
+def addBlock(self,data):
+   prevHash = chain[-1].hash #hash of last block of blockchain
+   hash = genHash(data+prevHash)
+   block = Block(data,hash,prevHash)
